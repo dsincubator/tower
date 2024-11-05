bc1ef940e8387298c357c97f2f46659b1848f5b2
Mauro Lepore <maurolepore@gmail.com>
2024-11-05 22:33:10 +0000

    New check for data frame (#1)

--- tests/testthat/test-do_nothing.R
+++ tests/testthat/test-do_nothing.R
@@ -2,3 +2,7 @@ test_that("it's like identity", {
   data <- datasets::BOD
   expect_equal(do_nothing(data), identity(data))
 })
+
+test_that("it's like identity", {
+  expect_snapshot(do_nothing("a"), error = TRUE)
+})
